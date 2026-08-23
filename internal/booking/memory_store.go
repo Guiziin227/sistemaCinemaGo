529264061af8c53790ac0ce7e709825a58dabd61
Guiziin227 <gh63388@gmail.com>
2026-08-23 11:38:28 -0300

    add memory store implementation for booking management

---
+++ internal/booking/memory_store.go
@@ -0,0 +1,16 @@
+package booking
+
+type MemoryStore struct {
+	bookings map[string]Booking
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		bookings: map[string]Booking{},
+	}
+}
+
+func (s *MemoryStore) Book(b Booking) (Booking, error) {}
+func (s *MemoryStore) ListBookings(movieID string) []Booking {
+
+}
